cc6328c367bcdf3f1cf91c2bac31895c9b30e092
jupl <juan.placencia.512@gmail.com>
2015-12-01 18:20:14 -0500

    Update babel configuration

--- babel.js
+++ babel.js
@@ -5,11 +5,18 @@ var config = module.exports = require('./config/babel')
 var rules = [
   'arrow-parens',
   'generator-star-spacing',
+  'new-cap',
   'object-curly-spacing',
   'object-shorthand',
 ]
 
-rules.forEach(function(rule) {
+rules.filter(hasRuleInDefault).forEach(useBabelRule)
+
+function hasRuleInDefault(rule) {
+  return !!defaultConfig.rules[rule]
+}
+
+function useBabelRule(rule) {
   config.rules['babel/' + rule] = defaultConfig.rules[rule]
   config.rules[rule] = 0
-})
+}

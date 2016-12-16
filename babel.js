7633288e006ae313164f21e992dfbfd482570d6f
jupl <juan.placencia.512@gmail.com>
2016-12-15 18:24:25 -0600

    Update

--- babel.js
+++ babel.js
@@ -1,26 +1,3 @@
 'use strict'
 
-var configs = {
-  base: require('./config/base'),
-  babel: require('./config/babel'),
-}
-var rules = [
-  'array-bracket-spacing',
-  'arrow-parens',
-  'generator-star-spacing',
-  'new-cap',
-  'object-curly-spacing',
-  'object-shorthand',
-]
-module.exports = configs.babel
-
-rules.filter(hasRuleInBase).forEach(useBabelRule)
-
-function hasRuleInBase(rule) {
-  return !!configs.base.rules[rule]
-}
-
-function useBabelRule(rule) {
-  configs.babel.rules['babel/' + rule] = configs.base.rules[rule]
-  configs.base.rules[rule] = 0
-}
+module.exports = require('./config/babel')
